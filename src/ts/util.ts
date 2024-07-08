22f5735ba03c5bb9efb4c97229ad283cca06036f
orifu <orifu@duck.com>
2024-07-09 00:53:23 +0100

    cool mod info page

---
+++ src/ts/util.ts
@@ -0,0 +1,8 @@
+export function formatNumber(i: number): string {
+    let out = '';
+    while (i > 0) {
+        out = ('' + (i % 1000)).padStart(3, '0') + ',' + out;
+        i = Math.floor(i / 1000);
+    }
+    return out.slice(0, -1).replace(/^0+/, '');
+}
